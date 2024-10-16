e749c4568686a8d0bbe560b21d2d9f9c1831cac3
Caleb-Andreas <caleb.andreas@mths.ca>
2024-10-16 15:24:43 +0000

    started sonar code

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,10 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Caleb Andreas
+Created on: Oct 2024
+This module is a Micro:bit MicroPython program that can measure distance with a sonar.
 """
 
 from microbit import *
 
 
-display.scroll("Hello, World!")
+
